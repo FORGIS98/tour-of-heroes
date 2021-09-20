e85ee0b178201a4ce965e925e616a746cb96715e
FORGIS98 <jorgesolgonzalez1998@gmail.com>
2021-09-20 19:43:32 +0200

    Java backend initial commit

--- JavaBackEnd/src/main/java/com/heroes/api/ApiApplication.java
+++ JavaBackEnd/src/main/java/com/heroes/api/ApiApplication.java
@@ -1,4 +1,4 @@
-package com.tourofheroes.api;
+package com.heroes.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- JavaBackEnd/src/test/java/com/heroes/api/ApiApplicationTests.java
+++ JavaBackEnd/src/test/java/com/heroes/api/ApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.tourofheroes.api;
+package com.heroes.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
