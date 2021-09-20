1ec981286c19b97cf63b15ac86f5d6387dc18faa
FORGIS98 <jorgesolgonzalez1998@gmail.com>
2021-09-20 19:05:24 +0200

    Add Navigation

--- src/app/heroes-detail/heroes-detail.component.ts
+++ src/app/heroes-detail/heroes-detail.component.ts
@@ -1,18 +1,34 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes-detail',
   templateUrl: './heroes-detail.component.html',
-  styleUrls: ['./heroes-detail.component.css']
+  styleUrls: ['./heroes-detail.component.css'],
 })
 export class HeroesDetailComponent implements OnInit {
-
   @Input() hero?: Hero;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
 
   ngOnInit(): void {
+    this.getHero();
   }
 
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe((hero) => (this.hero = hero));
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
