d0bd54f1ef5b6a275fcf7043b8af56a793853385
FORGIS98 <jorgesolgonzalez1998@gmail.com>
2021-09-21 13:08:18 +0200

    new HTTP requests

--- JavaBackEnd/src/main/java/com/heroes/api/HeroController.java
+++ JavaBackEnd/src/main/java/com/heroes/api/HeroController.java
@@ -1,6 +1,7 @@
 package com.heroes.api;
 
 import java.util.List;
+import java.util.ArrayList;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,17 +26,50 @@ public class HeroController {
         return repo.findAll();
     }
 
-    @PostMapping("/hero")
+    @PostMapping("/heroes")
     Hero newHero(@RequestBody Hero newHero) {
         return repo.save(newHero);
     }
 
-    @GetMapping("/hero/{id}")
+    @PutMapping("/heroes/{id}")
+    Hero updateHero(@RequestBody Hero newHero, @PathVariable int id) {
+        return repo.findById(id).map(h -> {
+            h.setName(newHero.getName());
+            return repo.save(h);
+        }).orElseGet(() -> {
+            newHero.setId(id);
+            return repo.save(newHero);
+        });
+    }
+
+    @GetMapping("/heroes/{id}")
     Hero oneHero(@PathVariable int id) {
         return repo.findById(id).orElseThrow(() -> new HeroNotFoundException(id));
     }
 
-    @DeleteMapping("/hero/{id}")
+    @GetMapping("/heroes/")
+    List<Hero> searchHero(@RequestParam(name = "name", required = true) String name) {
+
+        System.out.println("\n\n");
+        System.out.println(name);
+        System.out.println("\n\n");
+
+        List<Hero> heroesSearch = new ArrayList<Hero>();
+
+        System.out.println("\n\n");
+        System.out.println(heroesSearch.toString());
+        System.out.println("\n\n");
+
+        for(Hero h: repo.findAll()) {
+            if(h.getName().contains(name)){
+                heroesSearch.add(h);
+            }
+        }
+
+        return heroesSearch;
+    }
+
+    @DeleteMapping("/heroes/{id}")
     void deleteHero(@PathVariable int id) {
         repo.deleteById(id);
     }
